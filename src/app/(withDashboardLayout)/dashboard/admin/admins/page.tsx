c77e21c4cbc3358b68eaab5653d5ad8007230a57
sakib <mohammadsakib7679@gmail.com>
2024-11-23 13:03:07 +0600

    added data grid

--- src/app/(withDashboardLayout)/dashboard/admin/admins/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/admins/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import CircularProgress from "@mui/material/CircularProgress";
 import AdminModal from "./components/AdminModal";
 import { useState } from "react";
 import { useGetAdminsQuery } from "@/redux/api/adminApi";
 
+const columns: GridColDef[] = [
+  { field: "firstName", headerName: "First Name", width: 150 },
+  { field: "lastName", headerName: "Last Name", width: 150 },
+  { field: "email", headerName: "Email", width: 200 },
+];
+
 const AdminsPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { data, isLoading } = useGetAdminsQuery({});
+  const { data, isLoading, error } = useGetAdminsQuery({});
 
+  // Handle different states
   if (isLoading) {
-    <Typography>Loaidng</Typography>;
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    );
   }
+
+  if (!data || data.length === 0) {
+    return (
+      <Box>
+        <Typography variant="h6">No admins found!</Typography>
+      </Box>
+    );
+  }
+  const paginationModel = { page: 0, pageSize: 5 };
+
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -20,7 +44,18 @@ const AdminsPage = () => {
         <AdminModal open={isModalOpen} setOpen={setIsModalOpen} />
         <TextField placeholder="Search Admin"></TextField>
       </Stack>
-      <Box>{data?.length}</Box>
+      <Box mt={4}>
+        <Paper sx={{ height: "100%", width: "100%" }}>
+          <DataGrid
+            rows={data}
+            columns={columns}
+            initialState={{ pagination: { paginationModel } }}
+            pageSizeOptions={[10, 20]}
+            checkboxSelection
+            sx={{ border: 0 }}
+          />
+        </Paper>
+      </Box>
     </Box>
   );
 };

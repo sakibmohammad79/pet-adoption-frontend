ab944d50d46fd984870ddfb63cbcee5af6aaaff8
sakib <mohammadsakib7679@gmail.com>
2024-11-23 14:09:13 +0600

    display admin data

--- src/app/(withDashboardLayout)/dashboard/admin/admins/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/admins/page.tsx
@@ -6,16 +6,17 @@ import CircularProgress from "@mui/material/CircularProgress";
 import AdminModal from "./components/AdminModal";
 import { useState } from "react";
 import { useGetAdminsQuery } from "@/redux/api/adminApi";
-
-const columns: GridColDef[] = [
-  { field: "firstName", headerName: "First Name", width: 150 },
-  { field: "lastName", headerName: "Last Name", width: 150 },
-  { field: "email", headerName: "Email", width: 200 },
-];
+import Image from "next/image";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const AdminsPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { data, isLoading, error } = useGetAdminsQuery({});
+
+  const handleDelete = (id: string) => {
+    console.log(id);
+  };
 
   // Handle different states
   if (isLoading) {
@@ -33,6 +34,97 @@ const AdminsPage = () => {
       </Box>
     );
   }
+
+  const columns: GridColDef[] = [
+    {
+      field: "profilePhoto",
+      headerName: "Image",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: ({ row }) => {
+        if (row?.profilePhoto) {
+          return (
+            <Box>
+              <Image
+                src={row?.profilePhoto}
+                alt="profile"
+                height={30}
+                width={30}
+              />
+            </Box>
+          );
+        }
+        return (
+          <Box>
+            <Image
+              src="https://i.postimg.cc/6qRH1Y3S/profile-icon.png"
+              alt="profile"
+              height={30}
+              width={30}
+            />
+          </Box>
+        );
+      },
+    },
+    {
+      field: "firstName",
+      headerName: "First Name",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "gender",
+      headerName: "Gender",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "contactNumber",
+      headerName: "Contact Number",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      align: "center",
+      headerAlign: "center",
+      renderCell: ({ row }) => {
+        return (
+          <IconButton onClick={() => handleDelete(row?.id)} aria-label="delete">
+            <DeleteIcon sx={{ color: "red" }} />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
   const paginationModel = { page: 0, pageSize: 5 };
 
   return (
@@ -50,7 +142,7 @@ const AdminsPage = () => {
             rows={data}
             columns={columns}
             initialState={{ pagination: { paginationModel } }}
-            pageSizeOptions={[10, 20]}
+            pageSizeOptions={[5, 10]}
             checkboxSelection
             sx={{ border: 0 }}
           />

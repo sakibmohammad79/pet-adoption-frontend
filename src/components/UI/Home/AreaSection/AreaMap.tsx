236b267d18b9d22fb910f3b0acb5c4ccec561e28
sakib <mohammadsakib7679@gmail.com>
2025-01-29 23:57:30 +0600

    Create map and area section components

---
+++ src/components/UI/Home/AreaSection/AreaMap.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: 22.3569,
+  lng: 91.7832,
+};
+
+const AreaMap = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading Maps...</div>;
+
+  return (
+    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={10} center={center}>
+      <Marker position={center} />
+    </GoogleMap>
+  );
+};
+
+export default AreaMap;
---
+++ src/components/UI/Home/AreaSection/AreaSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+// import ChattogramMap from '../components/ChattogramMap';
+
+const AreaSection = () => {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Chattogram District Map
+      </Typography>
+      {/* <ChattogramMap /> */}
+    </Container>
+  );
+};
+
+export default AreaSection;

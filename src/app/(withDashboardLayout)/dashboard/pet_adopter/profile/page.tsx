ea4522d10d2ad0e3f9ab35d9f80c38edd353aa85
sakib <mohammadsakib7679@gmail.com>
2025-02-07 23:34:50 +0600

    create pet adopter profile page

---
+++ src/app/(withDashboardLayout)/dashboard/pet_adopter/profile/page.tsx
@@ -0,0 +1,9 @@
+const PetAdopterProfile = () => {
+  return (
+    <div>
+      <h2>This is adopter profile page component. Comming Soon!!</h2>
+    </div>
+  );
+};
+
+export default PetAdopterProfile;

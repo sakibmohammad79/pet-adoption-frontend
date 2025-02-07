3c77396fc77c10587b55945e035836bbd5ca678a
sakib <mohammadsakib7679@gmail.com>
2025-02-07 23:36:25 +0600

    create pet publisher profile components

---
+++ src/app/(withDashboardLayout)/dashboard/pet_publisher/profile/page.tsx
@@ -0,0 +1,9 @@
+const PetPublisherProfile = () => {
+  return (
+    <div>
+      <h2>This is pet publisher profile page component. Comming Soon!!</h2>
+    </div>
+  );
+};
+
+export default PetPublisherProfile;
